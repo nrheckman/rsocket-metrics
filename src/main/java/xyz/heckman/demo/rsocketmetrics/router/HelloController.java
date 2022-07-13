8e38def6024e3904d37b2d58302eb2d0ccc1af44
Nickolas Heckman <nickolas@afero.io>
2022-07-13 16:59:43 -0700

    Adds hacked on manual metrics publishing

--- src/main/java/xyz/heckman/demo/rsocketmetrics/router/HelloController.java
+++ src/main/java/xyz/heckman/demo/rsocketmetrics/router/HelloController.java
@@ -1,24 +1,46 @@
 package xyz.heckman.demo.rsocketmetrics.router;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.SignalType;
 
 import java.io.Serializable;
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicLong;
 
-@Slf4j
 @Controller
-public class HelloController {
+@Slf4j
+@RequiredArgsConstructor
+public class HelloController implements InitializingBean {
+
+	/**
+	 * Non-ideal solution to get some kind of metrics out of rsocket interactions
+	 * Ideally, these would be added by a method interceptor maybe? Or a customization of the method handler?
+	 */
+	private final MeterRegistry meterRegistry;
+	private Timer successTimer;
+	private Timer failureTimer;
+
+	@Override
+	public void afterPropertiesSet() {
+		successTimer = meterRegistry.timer("rsocket.interactions", "type", "request-response", "path", "greetings.{lang}", "result", "success");
+		failureTimer = meterRegistry.timer("rsocket.interactions", "type", "request-response", "path", "greetings.{lang}", "result", "failure");
+	}
 
 	@MessageMapping("greetings.{lang}")
 	public Mono<HelloDto> encrypt(@DestinationVariable String lang, @Payload Mono<NameDto> nameMono) {
-		return nameMono
+		return nameMono.transform(mono -> addTimerMetrics(successTimer, failureTimer, mono))
 				.doOnNext(nameDto -> log.info("Got new message: {}", nameDto))
 				.flatMap(nameDto -> {
 					switch (lang.toLowerCase()) {
@@ -28,6 +50,20 @@ public class HelloController {
 									.build());
 						default:
 							return Mono.error(new IllegalArgumentException("Unsupported language requested"));
+					}
+				});
+	}
+
+	private <T> Mono<T> addTimerMetrics(Timer successTimer, Timer failureTimer, Mono<T> mono) {
+		final AtomicLong start = new AtomicLong();
+		return mono
+				.doFirst(() -> start.set(System.nanoTime()))
+				.doFinally(signalType -> {
+					Duration duration = Duration.ofNanos(System.nanoTime() - start.get());
+					if (signalType == SignalType.ON_ERROR || signalType == SignalType.CANCEL) {
+						failureTimer.record(duration);
+					} else {
+						successTimer.record(duration);
 					}
 				});
 	}
